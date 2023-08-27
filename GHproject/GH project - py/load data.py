3ec87e9371a49228ef5864c410de39e6dfbe333a
JongHee <jonghee1998@gmail.com>
2023-08-27 12:06:02 -0400

    add fs data

--- GHproject/GH project - py/load data.py
+++ GHproject/GH project - py/load data.py
@@ -9,10 +9,18 @@ today = datetime.today().strftime('%Y-%m-%d') # 현재 날짜 가져오기
 df_krx = fdr.StockListing('KRX')
 
 df = fdr.DataReader('005930','2020-01-02', today) # df = fdr.DataReader('종목코드','시작일자','종료일자')
-df
+print(df)
 
 save_path = '/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/data/stock data.csv'  # 파일 저장 경로 설정
-df.to_csv(save_path, index=False) # 데이터프레임을 CSV 파일로 저장
+df.to_csv(save_path, index=True) # 데이터프레임을 CSV 파일로 저장
 
 
 ######################################################## LOAD Financial Statement Data ########################################################
+
+from pykrx import stock
+
+df2 = stock.get_market_fundamental_by_date("20200102", today, "005930")
+print(df2)
+
+save_path = '/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/data/fs data.csv'  # 파일 저장 경로 설정
+df2.to_csv(save_path, index=True) # 데이터프레임을 CSV 파일로 저장
