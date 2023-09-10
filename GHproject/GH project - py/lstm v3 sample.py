b8d2dfdc3ace71a94da727a14ac1b5f7a71c72fa
JongHee <jonghee1998@gmail.com>
2023-09-10 00:27:41 -0400

    ㅕㅔㅇ

--- GHproject/GH project - py/lstm v3 sample.py
+++ GHproject/GH project - py/lstm v3 sample.py
@@ -1,4 +1,5 @@
 '''
+# https://github.com/neowizard2018/neowizard/blob/master/TensorFlow2/TF_2_x_LEC_21_LSTM_Example.ipynb
 ### 시계열 데이터에 생성된 주가 예측하기
 ### 사용 알고리즘: LSTM
 ### 수정종가 예측하는것이 목표
@@ -11,6 +12,9 @@
 ## 4. Construct LSTM (RNN) structure and train
 '''
 # Make sure that you have all these libaries available to run the code successfully
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 from pandas_datareader import data
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -38,11 +42,55 @@ df['MA20'] = df['Close'].rolling(window=20).mean()  # 20일 이평선 추가
 df['MA30'] = df['Close'].rolling(window=30).mean()  # 50일 이평선 추가
 
 df['Date'] = pd.to_datetime(df['Date'])
-df.set_index('Date', inplace=True)
+#df.set_index('Date', inplace=True)
 
 df.head()
 len(df) # 914
 
+######################################################################################################################################################################################
+# 원하는 날짜 구간 입력 받기
+start_date = pd.to_datetime(input("Enter the start date (YYYY-MM-DD): "))
+end_date = pd.to_datetime(input("Enter the end date (YYYY-MM-DD): "))
+
+# 입력 받은 날짜로 데이터 필터링
+filtered_df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+print(filtered_df)
+# 양봉과 음봉 색상 설정
+colors = ['red' if close > open else 'blue' for close, open in zip(filtered_df['Close'], filtered_df['Open'])]
+
+# 2x1 서브플롯 생성
+fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.03,
+                    row_heights=[0.7, 0.3],
+                    subplot_titles=('Stock Price', 'Volume'))
+
+# 캔들스틱 차트 추가
+fig.add_trace(go.Candlestick(x=filtered_df['Date'],
+                             open=filtered_df['Open'],
+                             high=filtered_df['High'],
+                             low=filtered_df['Low'],
+                             close=filtered_df['Close'],
+                             name='Candlesticks'), row=1, col=1)
+
+# 이평선 추가
+fig.add_trace(go.Scatter(x=filtered_df['Date'], y=filtered_df['MA5'], mode='lines', name='MA5', line=dict(color='red', width=1)), row=1, col=1)
+fig.add_trace(go.Scatter(x=filtered_df['Date'], y=filtered_df['MA10'], mode='lines', name='MA10', line=dict(color='blue', width=1)), row=1, col=1)
+fig.add_trace(go.Scatter(x=filtered_df['Date'], y=filtered_df['MA20'], mode='lines', name='MA20', line=dict(color='purple', width=1)), row=1, col=1)
+fig.add_trace(go.Scatter(x=filtered_df['Date'], y=filtered_df['MA30'], mode='lines', name='MA30', line=dict(color='green', width=1)), row=1, col=1)
+
+# 거래량 바 추가
+fig.add_trace(go.Bar(x=filtered_df['Date'], y=filtered_df['Volume'], name='Volume', marker_color=colors), row=2, col=1)
+
+# 차트 레이아웃 설정
+fig.update_layout(title='Stock Price Candlestick Chart with Volume for Selected Dates',
+                  xaxis_title='Date',
+                  yaxis_title='Stock Price',
+                  xaxis_rangeslider_visible=False)
+
+# 차트 보이기
+fig.show()
+
+
+######################################################################################################################################################################################
 
 ## 2. Data Preprocessing
 ##   - 1. Remove Outliers & Missing value 
@@ -92,7 +140,7 @@ label_np = label_df.to_numpy()
 
 print(feature_np.shape, label_np.shape) # (795, 6) (795, 1)
 
-
+######################################################################################################################################################################################
 ## 3. Create data
 ##  - 1. Set window size
 ##  - 2. Create Train data (As form 3D tensor - include batch size, time steps, input dims)
@@ -113,14 +161,18 @@ y_test = Y[split:]
 print(x_train.shape, y_train.shape) # (645, 50, 6) (617, 1)
 print(x_test.shape, y_test.shape) # (200, 40, 5) (200, 1)
 
-
+######################################################################################################################################################################################
 ## 4. Construct and Compile model
 
 # model 생성
 model = Sequential()
 
-model.add(LSTM(128, # LSTM 계층에 tanh를 activation function으로 가지는 node 수 128개, recurrent_activation은 코드에 명시되지 않았기 때문에 기본값인 'sigmoid'가 Forget 게이트와 Input 게이트의 Relevance 게이트에 사용됩니다.
-               activation='tanh', input_shape=x_train[0].shape)) # (40,5) 형태로 들어가게 된다
+model.add(LSTM(128, activation='tanh', input_shape=x_train[0].shape, return_sequences=True))  # return_sequences를 True로 설정하여 다음 LSTM 층으로 출력을 전달
+model.add(Dropout(0.2))  
+
+model.add(LSTM(64, activation='relu'))
+model.add(Dropout(0.2))  
+
 model.add(Dense(1, activation='linear')) # 출력층
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 
@@ -135,7 +187,7 @@ model.fit(x_train, y_train,
           validation_data=(x_test, y_test),
           epochs=100, batch_size=16,        # 100번 학습 - loss가 점점 작아진다, 만약 100번의 학습을 다 하지 않더라도 loss 가 더 줄지 않는다면, 맞춰둔 조건에 따라 조기종료가 이루어진다
           callbacks=[early_stop])
-
+######################################################################################################################################################################################
 # Prediction with Visualization
 pred = model.predict(x_test)
 
@@ -149,7 +201,7 @@ plt.grid()
 plt.legend(loc='best')
 
 plt.show()
-
+######################################################################################################################################################################################
 # 평균절대값백분율오차계산 (MAPE)
 mape = np.sum(abs(y_test - pred) / y_test) / len(x_test)
 mae = np.mean(np.abs(y_test - pred))
