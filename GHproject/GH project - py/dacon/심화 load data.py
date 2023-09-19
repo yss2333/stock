f23ef32b54152d9e17220ad637b016d1ee24bb15
JongHee <jonghee1998@gmail.com>
2023-09-19 18:35:55 -0400

    상관관계 파악 내용 추가

--- GHproject/GH project - py/dacon/심화 load data.py
+++ GHproject/GH project - py/dacon/심화 load data.py
@@ -10,8 +10,8 @@ from datetime import datetime
 
 
 # 0. 여기만 입력하세요.
-ticker = 'aapl' # 소문자로 입력해야 합니다 아니면 FS 뽑을때 오류
-start_date = '2020-01-02'
+ticker = 'tsla' # 소문자로 입력해야 합니다 아니면 FS 뽑을때 오류
+start_date = '2013-09-28'
 end_date = '2023-09-08'
 
 
@@ -50,7 +50,7 @@ stock_df['OBV'] = ta.volume.on_balance_volume(stock_df['Close'], stock_df['Volum
 
 tech_df = stock_df
 
-save_path = f'/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/{ticker}_Tech_stock_data.csv'  
+save_path = f'/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/{ticker}_stock_Tech_data.csv'  
 tech_df.to_csv(save_path, index=True) 
 
 ########################################################### load Economic Indicator DATA ########################################################### 
@@ -98,21 +98,46 @@ FSI.index.name = 'Date'
 
 # 모든 결합된 데이터를 합침
 econ_df = adj_close_df.join([DGS, T10Y2Y, VIX, FSI], how='left')
+econ_df.index = pd.to_datetime(econ_df.index)
+econ_df.dtypes # Check which Data types are object
 
 save_path = '/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/Econ_data.csv'  
 econ_df.to_csv(save_path, index=True) 
 
-
 ########################################################### load Industry Indicator DATA ########################################################### 
 import yfinance as yf
 import pandas as pd
+from pandas_datareader import data as pdr
+import yfinance as yfin
 
 ###
 # 생성되는 변수: 다우존스지수, S&P500, 나스닥, 러셀2000, etf 기반 산업별 수정종가 변화, etf 기반 산업별 거래량 변화
 ###
 
+## Load 4 index data
+yfin.pdr_override()
 
-### 태환부분 코드 없음
+df = pdr.get_data_yahoo("^DJI", start=start_date, end=end_date) # 다우지수
+df1 = pdr.get_data_yahoo("NDAQ", start=start_date, end=end_date) # 나스닥
+df2 = pdr.get_data_yahoo("^SPX", start=start_date, end=end_date) # S&P500
+df3 = pdr.get_data_yahoo("^RUT", start=start_date, end=end_date) # 러셀 2000
+
+df.rename(columns={'Adj Close': 'DJI Adj Close', 'Volume': 'DJI Volume'}, inplace=True) # 이름 다시정하기
+df1.rename(columns={'Adj Close': 'NDAQ Adj Close', 'Volume': 'NDAQ Volume'}, inplace=True)
+df2.rename(columns={'Adj Close': 'SPX Adj Close', 'Volume': 'SPX Volume'}, inplace=True)
+df3.rename(columns={'Adj Close': 'RUT Adj Close', 'Volume': 'RUT Volume'}, inplace=True)
+
+df = df[['DJI Adj Close', 'DJI Volume']] # 각 자료 칼럼 선택
+df1 = df1[['NDAQ Adj Close', 'NDAQ Volume']]
+df2 = df2[['SPX Adj Close', 'SPX Volume']]
+df3 = df3[['RUT Adj Close', 'RUT Volume']]
+
+Index_data = pd.concat([df, df1, df2, df3], axis=1, join='outer')
+
+
+Index_data = Index_data.join(stock_df['Adj Close'], how='left')
+save_path = '/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/Index_data.csv'  
+Index_data.to_csv(save_path, index=True) 
 
 ## ETF 기반 산업별 수정종가 + 거래량 데이터
 sectors = {
@@ -139,7 +164,7 @@ for sector, sector_name in sectors.items():
     sector_data[sector] = data[[f'{sector_name} Adj Close', f'{sector_name} Volume']]
 
 ETF = pd.concat(sector_data.values(), axis=1)
-
+ETF.index = pd.to_datetime(ETF.index)
 
 save_path = '/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/GICS_sector.csv'  
 ETF.to_csv(save_path, index=True) 
@@ -160,43 +185,139 @@ from yahooquery import Ticker
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
 }
-
+## 1. INCOME STATEMENT
 url = f"https://stockanalysis.com/stocks/{ticker}/financials/?p=quarterly" 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'html.parser')
 element_tables = soup.select("table[data-test='financials']")
 
-df = pd.read_html(str(element_tables))[0] #'0번 테이블 뽑기
-df.to_csv(ticker+'.csv', index=False)
-df
+Income_df = pd.read_html(str(element_tables))[0] #'0번 테이블 뽑기
+Income_df.to_csv(ticker+'.csv', index=False)
 
-FS_quarter = df.transpose()
-FS_quarter.columns = FS_quarter.iloc[0]
-FS_quarter = df.set_index("Quarter Ended").transpose()
-FS_quarter.index.name = "Date"
-FS_quarter.to_csv(ticker+'.csv', index=True, encoding='euc-kr')
-FS_quarter = FS_quarter.iloc[:-1, :]
-FS_quarter.index = pd.to_datetime(FS_quarter.index)
-FS_quarter
-save_path = f'/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/{ticker}_FS_quarter.csv'  
-FS_quarter.to_csv(save_path, index=True) 
+FS_Income = Income_df.transpose()
+FS_Income.columns = FS_Income.iloc[0]
+FS_Income = Income_df.set_index("Quarter Ended").transpose()
+FS_Income.index.name = "Date"
+FS_Income.to_csv(ticker+'.csv', index=True, encoding='euc-kr')
+FS_Income = FS_Income.iloc[:-1, :]
 
+for column in FS_Income.columns:
+    if FS_Income[column].dtype == 'object':       
+        FS_Income[column] = FS_Income[column].apply(lambda x: float(x) if '-' in x and x[1:].isdigit() else x)             # '-' 뒤에 숫자가 있는 문자열 처리
+        if FS_Income[column].str.contains('%').any():
+            FS_Income[column] = FS_Income[column].apply(lambda x: float(x.replace('%', '')) / 100 if '%' in x else x) # 퍼센트 기호가 있는 문자열 처리       
+        FS_Income[column] = pd.to_numeric(FS_Income[column], errors='coerce') # 다른 문자열을 숫자로 변환
+
+FS_Income = FS_Income.astype(float)
+FS_Income.index = pd.to_datetime(FS_Income.index)
+FS_Income.dtypes # Check which Data types are object
+FS_Income = FS_Income.join(stock_df['Adj Close'], how='left')
+
+FS_Income
+save_path = f'/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/{ticker}_FS_Income.csv'  
+FS_Income.to_csv(save_path, index=True) 
+
+## 2. RATIO STATEMENT
+url = f"https://stockanalysis.com/stocks/{ticker}/financials/ratios/?p=quarterly" 
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.content, 'html.parser')
+element_tables = soup.select("table[data-test='financials']")
+
+Ratio_df = pd.read_html(str(element_tables))[0] #'0번 테이블 뽑기
+Ratio_df.to_csv(ticker+'.csv', index=False)
+
+FS_Ratio = Ratio_df.transpose()
+FS_Ratio.columns = FS_Ratio.iloc[0]
+FS_Ratio = Ratio_df.set_index("Quarter Ended").transpose()
+FS_Ratio.index.name = "Date"
+FS_Ratio.to_csv(ticker+'.csv', index=True, encoding='euc-kr')
+FS_Ratio = FS_Ratio.iloc[1:-1, :]
+
+
+for column in FS_Ratio.columns:
+    if FS_Ratio[column].dtype == 'object':       
+        FS_Ratio[column] = FS_Ratio[column].apply(lambda x: float(x) if '-' in x and x[1:].isdigit() else x)             # '-' 뒤에 숫자가 있는 문자열 처리
+        if FS_Ratio[column].str.contains('%').any():
+            FS_Ratio[column] = FS_Ratio[column].apply(lambda x: float(x.replace('%', '')) / 100 if '%' in x else x) # 퍼센트 기호가 있는 문자열 처리       
+        FS_Ratio[column] = pd.to_numeric(FS_Ratio[column], errors='coerce') # 다른 문자열을 숫자로 변환
+
+FS_Ratio = FS_Ratio.astype(float)
+FS_Ratio.index = pd.to_datetime(FS_Ratio.index)
+FS_Ratio.dtypes # Check which Data types are object
+FS_Ratio = FS_Ratio.join(stock_df['Adj Close'], how='left')
+
+FS_Ratio
+save_path = f'/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/{ticker}_FS_Ratio.csv'  
+FS_Ratio.to_csv(save_path, index=True) 
+
+
+## 3. Balance Sheet
+url = f"https://stockanalysis.com/stocks/{ticker}/financials/balance-sheet/?p=quarterly" 
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.content, 'html.parser')
+element_tables = soup.select("table[data-test='financials']")
+
+Balance_df = pd.read_html(str(element_tables))[0] #'0번 테이블 뽑기
+Balance_df.to_csv(ticker+'.csv', index=False)
+
+FS_Balance = Balance_df.transpose()
+FS_Balance.columns = FS_Balance.iloc[0]
+FS_Balance = Balance_df.set_index("Quarter Ended").transpose()
+FS_Balance.index.name = "Date"
+FS_Balance.to_csv(ticker+'.csv', index=True, encoding='euc-kr')
+FS_Balance = FS_Balance.iloc[:-1, :]
+FS_Balance.index = pd.to_datetime(FS_Balance.index)
+
+for column in FS_Balance.columns:
+    if FS_Balance[column].dtype == 'object':       
+        FS_Balance[column] = FS_Balance[column].apply(lambda x: float(x) if '-' in x and x[1:].isdigit() else x)             # '-' 뒤에 숫자가 있는 문자열 처리
+        if FS_Balance[column].str.contains('%').any():
+            FS_Balance[column] = FS_Balance[column].apply(lambda x: float(x.replace('%', '')) / 100 if '%' in x else x) # 퍼센트 기호가 있는 문자열 처리       
+        FS_Balance[column] = pd.to_numeric(FS_Balance[column], errors='coerce') # 다른 문자열을 숫자로 변환
+
+FS_Balance = FS_Balance.astype(float)
+FS_Balance.index = pd.to_datetime(FS_Balance.index)
+FS_Balance.dtypes # Check which Data types are object
+FS_Balance = FS_Balance.join(stock_df['Adj Close'], how='left')
+
+FS_Balance
+save_path = f'/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/{ticker}_FS_Balance.csv'  
+FS_Balance.to_csv(save_path, index=True) 
+
+## 4. Cash Flow
+url = f"https://stockanalysis.com/stocks/{ticker}/financials/cash-flow-statement/?p=quarterly" 
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.content, 'html.parser')
+element_tables = soup.select("table[data-test='financials']")
+
+Cash_df = pd.read_html(str(element_tables))[0] #'0번 테이블 뽑기
+Cash_df.to_csv(ticker+'.csv', index=False)
+
+FS_Cash = Cash_df.transpose()
+FS_Cash.columns = FS_Cash.iloc[0]
+FS_Cash = Cash_df.set_index("Quarter Ended").transpose()
+FS_Cash.index.name = "Date"
+FS_Cash.to_csv(ticker+'.csv', index=True, encoding='euc-kr')
+FS_Cash = FS_Cash.iloc[:-1, :]
+
+for column in FS_Cash.columns:
+    if FS_Cash[column].dtype == 'object':       
+        FS_Cash[column] = FS_Cash[column].apply(lambda x: float(x) if '-' in x and x[1:].isdigit() else x)             # '-' 뒤에 숫자가 있는 문자열 처리
+        if FS_Cash[column].str.contains('%').any():
+            FS_Cash[column] = FS_Cash[column].apply(lambda x: float(x.replace('%', '')) / 100 if '%' in x else x) # 퍼센트 기호가 있는 문자열 처리       
+        FS_Cash[column] = pd.to_numeric(FS_Cash[column], errors='coerce') # 다른 문자열을 숫자로 변환
+
+FS_Cash = FS_Cash.astype(float)
+FS_Cash.index = pd.to_datetime(FS_Cash.index)
+FS_Cash.dtypes # Check which Data types are object
+FS_Cash = FS_Cash.join(stock_df['Adj Close'], how='left')
+
+FS_Cash
+save_path = f'/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/{ticker}_FS_Cash.csv'  
+FS_Cash.to_csv(save_path, index=True) 
+
+#######################################################################################################
 
 
 ###################################### Merge into Fundamental analysis data ###########################################
-info =yf.Ticker(ticker).info
-sector = info.get('sector', None)
-sector
-
-sector_columns =  [col for col in ETF.columns if sector in col] 
-sector_df = ETF[sector_columns]
-sector_df.head()
-
-merge_df = econ_df.merge(sector_df, on="Date", how="outer")
-fund_df = merge_df.merge(FS_quarter, on="Date", how="outer")
-fund_df = fund_df.sort_values(by='Date')
-
-fund_df
-save_path = f'/Users/jongheelee/Desktop/JH/personal/GHproject/GH project - py/dacon/심화 loaded data/{ticker}_Fund_stock_data.csv'  
-fund_df.to_csv(save_path, index=True) 
 
